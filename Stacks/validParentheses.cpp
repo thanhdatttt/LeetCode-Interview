76c81f58b43bd63582e31aa6604014d36aa53228
thanhdatttt <tmaxzv@gmail.com>
2025-12-09 08:41:43 +0700

    add stack problems

---
+++ Stacks/validParentheses.cpp
@@ -0,0 +1,40 @@
+// Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', 
+// determine if the input string is valid.
+
+// An input string is valid if:
+
+// Open brackets must be closed by the same type of brackets.
+// Open brackets must be closed in the correct order.
+// Every close bracket has a corresponding open bracket of the same type.
+
+#include <iostream>
+#include <stack>
+using namespace std;
+
+bool isValid(string s) {
+    stack<char> st;
+
+    for (char c : s) {
+        if (c == '(' || c == '{' || c == '[') {
+            st.push(c);
+        } else {
+            if (st.empty()) {
+                return false;
+            }
+
+            char top = st.top(); st.pop();
+            if (c == ')' && top != '(') return false;
+            if (c == '}' && top != '{') return false;
+            if (c == ']' && top != '[') return false;
+        }
+    }
+
+    return st.empty();
+}
+
+int main() {
+    string s = "()[]{}";
+
+    cout << isValid(s) << endl;
+    return 0;
+}
